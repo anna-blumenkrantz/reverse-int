f2c0f59671a7e2227e8bd49c55eaee4458d2d8cb
anna-blumenkrantz <91938764+anna-blumenkrantz@users.noreply.github.com>
2023-02-05 19:20:49 +0200

    init: reverse done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let numStr = n.toString();
+  let reversed = '';
+  for (let i = numStr.length - 1; i >= 0; i--) {
+    reversed += numStr[i];
+  }
+  return parseInt(reversed);
 }
